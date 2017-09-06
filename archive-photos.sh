92a0a90ae429c1feee91d6ed41788199d5231a3c
Roberto Casati <casati.roberto@gmail.com>
2017-09-06 23:28:52 +0200

    refactoring

--- archive-photos.sh
+++ archive-photos.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-FILENAME_PATTERN="%Y-%m-%d/%Y-%m-%d_%H.%M.%S"
-#echo ${FILENAME_PATTERN}
 DIR="$1"
-
+echo "DIR=${DIR}"
 
 #remove duplicates
 #fdupes -rdN "${DIR}"
@@ -11,22 +9,28 @@ DIR="$1"
 #copy jpgs and apply naming convention
 
 PHASE1_DIR="archive-pictures.phase1"
+
 if [ -d "${PHASE1_DIR}" ]; then rm -rf "${PHASE1_DIR}"; fi
 mkdir "${PHASE1_DIR}"
 
-find "${DIR}" -iname \*.jpg -type f | while read FILE; do
-	echo ${FILE}
+archive () {
+	#archive <filename> <destination_dir>
+	FILE="$1"
+	DEST_DIR="$2"
+	echo "DD=${DEST_DIR}"
+	FILENAME_PATTERN="%Y-%m-%d/%Y-%m-%d_%H.%M.%S"
 	
 	COUNTER=0
 	COUNTER_PAD=$(printf "%03d\n" ${COUNTER})
-	TARGET_FILE_NAME_BASE=$(exiftool -json -dateformat "${PHASE1_DIR}/${FILENAME_PATTERN}" "${FILE}" | jq --raw-output '.[].DateTimeOriginal')
+	TARGET_FILE_NAME_BASE=$(exiftool -json -dateformat "${DEST_DIR}/${FILENAME_PATTERN}" "${FILE}" | jq --raw-output '.[].DateTimeOriginal')
+	echo "TFNB=${TARGET_FILE_NAME_BASE}"
 	
-	if [ ${TARGET_FILE_NAME_BASE} != "null" ]; then
+	if [ "${TARGET_FILE_NAME_BASE}" != "null" ]; then
 	
 		TARGET_FILE_NAME="${TARGET_FILE_NAME_BASE}-${COUNTER_PAD}.jpg"
-		echo "(${COUNTER}), ${TARGET_FILE_NAME}"
+		echo "C=${COUNTER}, TFN=${TARGET_FILE_NAME}"
 		
-		while [ -f ${TARGET_FILE_NAME} ]; do
+		while [ -f "${TARGET_FILE_NAME}" ]; do
 			COUNTER=$((${COUNTER} + 1))
 			COUNTER_PAD=$(printf "%03d\n" ${COUNTER})
 			echo ${COUNTER_PAD}
@@ -34,9 +38,51 @@ find "${DIR}" -iname \*.jpg -type f | while read FILE; do
 			echo "+1 (${COUNTER}), ${TARGET_FILE_NAME}"
 		done
 	
-		echo ${TARGET_FILE_NAME}
-		mkdir -p "${TARGET_FILE_NAME}" && cp "${FILE}" "${TARGET_FILE_NAME}"
+		echo "copying ${FILE} --> ${TARGET_FILE_NAME}"
+		echo mkdir -p "${TARGET_FILE_NAME}"
+		mkdir -p "${TARGET_FILE_NAME}"
+		cp "${FILE}" "${TARGET_FILE_NAME}"
+		
+		read -p "pausa"
+		
+	else
+		echo "nuuuuuuuuuuuuuuuuuuuuuulll"
+		echo ${FILE} >> errors
+	fi
+}
+export -f archive
 
+find "${DIR}" -iname '*.jpg' -type f -exec bash -c 'archive "{}" '"${PHASE1_DIR}" \;
+
+
+exit 0
+
+find "${DIR}" -iname '*.jpg' -type f | while read FILE; do
+	echo "FILE=${FILE}"
+	
+	COUNTER=0
+	COUNTER_PAD=$(printf "%03d\n" ${COUNTER})
+	TARGET_FILE_NAME_BASE=$(exiftool -json -dateformat "${PHASE1_DIR}/${FILENAME_PATTERN}" "${FILE}" | jq --raw-output '.[].DateTimeOriginal')
+	echo "TFNB=${TARGET_FILE_NAME_BASE}"
+	
+	if [ "${TARGET_FILE_NAME_BASE}" != "null" ]; then
+	
+		TARGET_FILE_NAME="${TARGET_FILE_NAME_BASE}-${COUNTER_PAD}.jpg"
+		echo "C=${COUNTER}, TFN=${TARGET_FILE_NAME}"
+		
+		while [ -f "${TARGET_FILE_NAME}" ]; do
+			COUNTER=$((${COUNTER} + 1))
+			COUNTER_PAD=$(printf "%03d\n" ${COUNTER})
+			echo ${COUNTER_PAD}
+			TARGET_FILE_NAME="${TARGET_FILE_NAME_BASE}-${COUNTER_PAD}.jpg"
+			echo "+1 (${COUNTER}), ${TARGET_FILE_NAME}"
+		done
+	
+		echo "copying ${FILE} --> ${TARGET_FILE_NAME}"
+		echo mkdir -p "${TARGET_FILE_NAME}"
+		mkdir -p "${TARGET_FILE_NAME}"
+		cp "${FILE}" "${TARGET_FILE_NAME}"
+		
 	else
 		echo "nuuuuuuuuuuuuuuuuuuuuuulll"
 		echo ${FILE} >> errors
