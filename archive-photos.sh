4b8bf6476369bb7af2005d7653a0c38d37a8ec8e
Roberto Casati <casati.roberto@gmail.com>
2017-09-09 15:21:56 +0200

    overwrite check

--- archive-photos.sh
+++ archive-photos.sh
@@ -6,11 +6,19 @@
 SRC_DIR="$1"
 TARGET_DIR="$2"
 
+if [ -d "${TARGET_DIR}" ]; then
+	read -p "Directory \"${TARGET_DIR}\" already exists: overwrite (Y/*)?" yn;
+	case $yn in
+		""|"Y"|"y") echo "yes";;
+		*) echo "no"; exit 0;;
+	esac
+fi
+
 # Copy every file in a temp dir
 
 WORK_DIR="$(mktemp -d -p . -t XXXXXXXX)"
 echo "Copying files into temporary folder: ${WORK_DIR}..."
-exiftool -o . -progress: -filename="${WORK_DIR}"/%d%f.%e -if '($filetype eq "JPEG")' -r "${SRC_DIR}"
+exiftool -o . -v0 -progress -filename="${WORK_DIR}"/%d%f.%e -if '($filetype eq "JPEG")' -r "${SRC_DIR}"
 
 #remove duplicates
 echo "Removing duplicates..."
@@ -19,11 +27,11 @@ fdupes -rdN "${WORK_DIR}"
 #Update any photo that doesn't have DateTimeOriginal to have it based on file modify date
 
 echo "Fixing pictures without exif data..."
-exiftool '-datetimeoriginal<filemodifydate' -if '(not $datetimeoriginal or ($datetimeoriginal eq "0000:00:00 00:00:00")) and ($filetype eq "JPEG")' -r "${WORK_DIR}"
+exiftool -v0 -progress '-datetimeoriginal<filemodifydate' -if '(not $datetimeoriginal or ($datetimeoriginal eq "0000:00:00 00:00:00")) and ($filetype eq "JPEG")' -r "${WORK_DIR}"
 
 #Backup images
 
 echo "Backup..."
-exiftool -o .  -progress: '-FileName<DateTimeOriginal' -if '($filetype eq "JPEG")' -d "${TARGET_DIR}/%Y-%m/%Y-%m-%d_%H.%M.%S%%-.2c.jpg" -r "${WORK_DIR}"
+exiftool -o . -v0 -progress '-FileName<DateTimeOriginal' -if '($filetype eq "JPEG")' -d "${TARGET_DIR}/%Y-%m/%Y-%m-%d_%H.%M.%S%%-.2c.jpg" -r "${WORK_DIR}"
 
 rm -rf "${WORK_DIR}"
