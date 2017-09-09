a0f11fdb5e168372b1b5a4710d3f25e1bb21f6f8
Roberto Casati <casati.roberto@gmail.com>
2017-09-10 01:09:43 +0200

    several fixes

--- archive-photos.sh
+++ archive-photos.sh
@@ -9,8 +9,8 @@ FILE_TYPE="$3"
 
 #set proper extension
 case ${FILE_TYPE} in
-	"JPEG") FILE_EXTENSION="jpg" ;;
-	"MP4") FILE_EXTENSION="mp4" ;;
+	"JPEG") FILE_EXTENSION="jpg"; DESIRED_MIME="image/jpeg" ;;
+	"MP4") FILE_EXTENSION="mp4"; DESIRED_MIME="video/mp4" ;;
 	*) echo "Supported file types: JPEG, MP4."; exit 0;;
 esac
 
@@ -29,7 +29,12 @@ fi
 
 WORK_DIR="$(mktemp -d -p . -t XXXXXXXX)"
 echo "Copying files into temporary folder: ${WORK_DIR}..."
-exiftool -o . -v0 -progress -filename="${WORK_DIR}"/%d%f.%e -if '($filetype eq "'${FILE_TYPE}'")' -r "${SRC_DIR}"
+#exiftool -o . -v0 -progress -filename="${WORK_DIR}"/%d%f.%e -if '($filetype eq "'${FILE_TYPE}'")' -r "${SRC_DIR}"
+
+export DESIRED_MIME
+export WORK_DIR
+export N=1
+find "${SRC_DIR}" -type f -exec bash -c 'MIME=$(file -bi "{}" | cut -d\; -f1); if [ "${MIME}" == "${DESIRED_MIME}" ]; then echo -n "."; cp --parents "{}" "${WORK_DIR}"; fi' \;
 
 #remove duplicates
 echo "Removing duplicates..."
@@ -38,11 +43,11 @@ fdupes -rdN "${WORK_DIR}"
 #Update any photo that doesn't have DateTimeOriginal to have it based on file modify date
 
 echo "Fixing pictures without exif data..."
-exiftool -v0 -progress '-datetimeoriginal<filemodifydate' -if '(not $datetimeoriginal or ($datetimeoriginal eq "0000:00:00 00:00:00")) and ($filetype eq "'${FILE_TYPE}'")' -r "${WORK_DIR}"
+exiftool -v0 -progress '-datetimeoriginal<CreateDate' -if '(not $datetimeoriginal or ($datetimeoriginal eq "0000:00:00 00:00:00")) and ($filetype eq "'${FILE_TYPE}'")' -r "${WORK_DIR}"
 
 #Backup images
 
 echo "Backup..."
-exiftool -o . -v0 -progress '-FileName<DateTimeOriginal' -if '($filetype eq "'${FILE_TYPE}'")' -d "${TARGET_DIR}/%Y-%m/%Y-%m-%d_%H.%M.%S%%-.2c.${FILE_EXTENSION}" -r "${WORK_DIR}"
+exiftool -v0 -progress '-FileName<DateTimeOriginal' -if '($filetype eq "'${FILE_TYPE}'")' -d "${TARGET_DIR}/%Y-%m/%Y-%m-%d_%H.%M.%S%%-.2c.${FILE_EXTENSION}" -r "${WORK_DIR}"
 
 rm -rf "${WORK_DIR}"
